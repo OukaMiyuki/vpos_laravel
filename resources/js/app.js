84fbd78f69162dbc2a9c6d6e79201558020c0820
OukaMiyuki <asukaloid.blog@gmail.com>
2024-06-19 12:23:48 +0700

    update vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+
+const app = createApp({});
+
+app.mount("#app");
+
+Vue.component('send-button-otp', require('./components/sendOtpButton.vue'))
---
+++ resources/js/components/sendOtpButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <form @submit="formSubmit">
+            <button v-if="show" type="submit" class="btn btn-success" style="width:100%;">Apply</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default{
+        mounted() {
+            console.log('Component Mounted!')
+        },
+        data(){
+            return {
+                'show': true
+            }
+        }
+    }
+</script>
