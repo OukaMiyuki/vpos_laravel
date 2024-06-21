5846948397e15ee182872c665c83fa3f76fc64e9
OukaMiyuki <asukaloid.blog@gmail.com>
2024-06-21 23:23:47 +0700

    test query

--- app/Http/Controllers/Auth/Admin/AdminController.php
+++ app/Http/Controllers/Auth/Admin/AdminController.php
@@ -2599,7 +2599,6 @@ class AdminController extends Controller {
     }
 
     public function adminDashboardSettlementHistory(){
-        config()->set('database.connections.mysql.strict', false);
         $settlement = SettlementHstory::select([
                                             DB::raw("(sum(nominal_settle)) as total_settle"),
                                             DB::raw("(sum(nominal_insentif_cashback)) as total_cashback"),
@@ -2608,6 +2607,8 @@ class AdminController extends Controller {
                                         ->orderBy('settlement_time_stamp', 'DESC')
                                         ->groupBy([
                                             DB::raw("DATE_FORMAT(settlement_time_stamp, '%d-%m-%Y')"),
+                                            'nominal_insentif_cashback',
+                                            'nominal_insentif_cashback'
                                         ])
                                         ->get();
         dd($settlement);
