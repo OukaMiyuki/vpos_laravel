8c6a7c5faf7e85fca86ba3dc1317a0494931e118
OukaMiyuki <asukaloid.blog@gmail.com>
2024-06-22 23:20:51 +0700

    testing query

--- app/Console/Commands/TestQueryCommand.php
+++ app/Console/Commands/TestQueryCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Carbon\Carbon;
+use App\Models\History;
 use App\Models\QrisWallet;
 use App\Models\HistoryCashbackAdmin;
 use App\Models\SettlementHstory;
@@ -29,25 +30,34 @@ class TestQueryCommand extends Command {
      * Execute the console command.
      */
     public function handle() {
-        $settlementPending = SettlementPending::where('settlement_pending_status', 0)->get();
-        if(!is_null($settlementPending) || !empty($settlementPending)){
-            foreach($settlementPending as $stlPending){
-                $qrisWalletTenant = QrisWallet::where('id_user', $stlPending->id)->where('email', $stlPending->email)->first();
-                if(!is_null($qrisWalletTenant) || empty($qrisWalletTenant)){
-                    echo "\n"."Qris tenant : ".$qrisWalletTenant->saldo."\n";
-                }
-            }
+        History::create([
+            'action' => 'Settlement Update Success!',
+            'id_user' => NULL,
+            'email' => NULL,
+            'lokasi_anda' => 'System Report',
+            'deteksi_ip' => 'System Report',
+            'log' => Carbon::now(),
+            'status' => 1
+        ]);
+        // $settlementPending = SettlementPending::where('settlement_pending_status', 0)->get();
+        // if(!is_null($settlementPending) || !empty($settlementPending)){
+        //     foreach($settlementPending as $stlPending){
+        //         $qrisWalletTenant = QrisWallet::where('id_user', $stlPending->id)->where('email', $stlPending->email)->first();
+        //         if(!is_null($qrisWalletTenant) || empty($qrisWalletTenant)){
+        //             echo "\n"."Qris tenant : ".$qrisWalletTenant->saldo."\n";
+        //         }
+        //     }
 
-            $historySettleCashback = HistoryCashbackPending::where('settlement_status', 0)->get();
-            foreach($historySettleCashback as $cashbackHistory){
-                HistoryCashbackAdmin::create([
-                    'id_invoice' => $cashbackHistory->id_invoice,
-                    'nominal_terima_mdr' => $cashbackHistory->nominal_terima_mdr
-                ]);
-                $cashbackHistory->update([
-                    'settlement_status' => 1
-                ]);
-            }
-        }
+        //     $historySettleCashback = HistoryCashbackPending::where('settlement_status', 0)->get();
+        //     foreach($historySettleCashback as $cashbackHistory){
+        //         HistoryCashbackAdmin::create([
+        //             'id_invoice' => $cashbackHistory->id_invoice,
+        //             'nominal_terima_mdr' => $cashbackHistory->nominal_terima_mdr
+        //         ]);
+        //         $cashbackHistory->update([
+        //             'settlement_status' => 1
+        //         ]);
+        //     }
+        // }
     }
 }
