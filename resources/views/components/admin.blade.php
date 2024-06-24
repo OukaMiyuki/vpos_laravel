4d53180c9fa10393c20e132967df1c9da0b83839
OukaMiyuki <asukaloid.blog@gmail.com>
2024-06-24 11:59:02 +0700

    testing

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('assets/images/logo/Logo2.png') }}">
