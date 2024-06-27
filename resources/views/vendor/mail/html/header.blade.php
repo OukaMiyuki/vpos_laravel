dbb7de4f75dac11be640d8fdb2ce831ce3440a6e
OukaMiyuki <asukaloid.blog@gmail.com>
2024-06-28 03:15:40 +0700

    fix mail view

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Visioner')
 <img src="https://i.imgur.com/OVONl5a.png" class="logo" alt="Visioner Logo">
 @else
 {{ $slot }}
