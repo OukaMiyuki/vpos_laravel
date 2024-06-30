8ca537d77548e567291c022f0e5136d44f534cf8
OukaMiyuki <asukaloid.blog@gmail.com>
2024-07-01 06:00:18 +0700

    test

--- resources/views/tenant/tenant_invoice_detail.blade.php
+++ resources/views/tenant/tenant_invoice_detail.blade.php
@@ -266,7 +266,7 @@
                             <div class="mt-4 mb-1">
                                 <div class="text-end d-print-none">
                                     <a href="{{route('tenant.pos.invoice.receipt', ['id' => $invoice->id])}}" class="btn btn-primary waves-effect waves-light" target="_blank"><i class="mdi mdi-printer me-1"></i> Print Nota</a>&nbsp;&nbsp;
-                                    <a href="" href="" id="kirimWaButton" data-id="{{ $invoice->id }}" data-bs-toggle="modal" data-bs-target="#kirimWaModal" class="btn btn-primary waves-effect waves-light" target="_blank"><i class="mdi mdi-printer me-1"></i> Kirim Nota</a>
+                                    <a href="" id="kirimWaButton" data-id="{{ $invoice->id }}" data-bs-toggle="modal" data-bs-target="#kirimWaModal" class="btn btn-primary waves-effect waves-light" target="_blank"><i class="mdi mdi-printer me-1"></i> Kirim Nota</a>
                                     @if (($invoice->jenis_pembayaran == "Qris") && (!empty($invoice->qris_data)) && ($invoice->status_pembayaran == 0))
                                         &nbsp;&nbsp;<a href=""  data-bs-toggle="modal" data-bs-target="#lihatqris" class="btn btn-primary waves-effect waves-light"><i class="mdi mdi-printer me-1"></i> Lihat Qris</a>
                                         <div class="modal fade" id="lihatqris" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
