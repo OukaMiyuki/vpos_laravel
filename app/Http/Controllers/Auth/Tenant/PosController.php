0d54981a8b656c611dffb3a40a9cdc7ee6d70a94
OukaMiyuki <asukaloid.blog@gmail.com>
2024-07-01 02:52:10 +0700

    testing

--- app/Http/Controllers/Auth/Tenant/PosController.php
+++ app/Http/Controllers/Auth/Tenant/PosController.php
@@ -8,7 +8,7 @@ use Carbon\Carbon;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Stevebauman\Location\Facades\Location;
 use Illuminate\Support\Facades\DB;
-use Barryvdh\DomPDF\Facade\Pdf;
+// use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\ProductStock;
 use App\Models\ShoppingCart;
 use App\Models\Invoice;
@@ -23,6 +23,7 @@ use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
 use App\Models\TenantQrisAccount;
 use Exception;
+use PDF;
 
 class PosController extends Controller {
 
