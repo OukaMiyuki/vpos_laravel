43a4b084fb582b5df7ecfa56198a8151de5c815d
OukaMiyuki <asukaloid.blog@gmail.com>
2024-07-01 07:31:12 +0700

    testing

--- app/Http/Controllers/Auth/Tenant/PosController.php
+++ app/Http/Controllers/Auth/Tenant/PosController.php
@@ -750,7 +750,7 @@ class PosController extends Controller {
         ];
         $data = [
             "api_key" => "apLiCx2p1xJNbi9fWrZFxSLeE1dJ2t",
-            'sender' => "6285179950178",
+            'sender' => "085179950178",
             'number' => "6285156719832",
             "media_type" => "image",
             "caption" => "Nota Pembayaran anda",
