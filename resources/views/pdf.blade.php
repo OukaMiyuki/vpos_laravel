68b6a7de482e09a6a49eaabdf93b75ca53bd019e
OukaMiyuki <asukaloid.blog@gmail.com>
2024-07-05 11:42:25 +0700

    test

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -218,26 +218,36 @@
                 {{-- <img src="logo.png"> --}}
             </div>
             <div id="company">
-                <h2 class="name">Company Name</h2>
-                <div>455 Foggy Heights, AZ 85004, US</div>
-                <div>(602) 519-0450</div>
-                <div><a href="mailto:company@example.com">company@example.com</a></div>
+                <h2 class="name">{{$storeTenant->name}}</h2>
+                <div>{{$storeTenant->alamat}}</div>
+                <div>{{$storeTenant->no_telp_toko}}</div>
+                {{-- <div><a href="mailto:company@example.com">company@example.com</a></div> --}}
+                <div>{{$storeTenant->website}}</div>
             </div>
             </div>
         </header>
         <br>
+        @php
+            $field = App\Models\TenantField::where('store_identifier', $storeTenant->store_identifier)->first();
+        @endphp
         <main>
             <div id="details" class="clearfix">
                 <div id="client">
                     <div class="to">INVOICE TO:</div>
-                    <h2 class="name">John Doe</h2>
-                    <div class="address">796 Silver Harbour, TX 79273, US</div>
-                    <div class="email"><a href="mailto:john@example.com">john@example.com</a></div>
+                    @if(!empty($field->id))
+                        @if ( !empty($invoice->invoiceField->content1) || !empty($invoice->invoiceField->content2) || !empty($invoice->invoiceField->content3) || !empty($invoice->invoiceField->content4) || !empty($invoice->invoiceField->content5))
+                            <h2 class="name">@if(!empty($invoice->invoiceField->content1)){{$invoice->invoiceField->content1}}@endif</h2>
+                            <div class="address">@if(!empty($invoice->invoiceField->content2)){{$invoice->invoiceField->content2}}@endif</div>
+                            <div class="address">@if(!empty($invoice->invoiceField->content3)){{$invoice->invoiceField->content3}}@endif</div>
+                            <div class="phone">@if(!empty($invoice->invoiceField->content4)){{$invoice->invoiceField->content4}}@endif</div>
+                            <div class="email">@if (!empty($invoice->invoiceField->content5))<a href="mailto:{{$invoice->invoiceField->content5}}">{{$invoice->invoiceField->content5}}</a>@endif</div>
+                        @endif
+                    @endif
                 </div>
                 <div id="invoice">
-                    <h1>INVOICE 3-2-1</h1>
-                    <div class="date">Date of Invoice: 01/06/2014</div>
-                    <div class="date">Due Date: 30/06/2014</div>
+                    <h1>INVOICE : {{$unvoice->nomor_invoice}}</h1>
+                    <div class="date">Tanggal Transaksi: {{\Carbon\Carbon::parse($invoice->tanggal_transaksi)->format('d-m-Y')}} {{\Carbon\Carbon::parse($invoice->created_at)->format('H:i:s')}}</div>
+                    <div class="date">Tanggal Pembayaran: 30/06/2014</div>
                 </div>
             </div>
             <table border="0" cellspacing="0" cellpadding="0">
