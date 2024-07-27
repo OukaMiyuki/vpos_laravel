c6e486b04a1d31496d7bdbbc8205314b16f8e246
OukaMiyuki <asukaloid.blog@gmail.com>
2024-07-27 12:24:26 +0700

    update settlement script

--- app/Console/Commands/QrisPendingWalletUpdate.php
+++ app/Console/Commands/QrisPendingWalletUpdate.php
@@ -101,12 +101,15 @@ class QrisPendingWalletUpdate extends Command {
             $action = "";
             if($dateCollection != 0){
                 $action = "Settlement pending due to Holiday | ".Carbon::now()->format('d-m-Y');
+                echo $action;
             }
             if($today->dayOfWeek == Carbon::SATURDAY){
                 $action = "Settlement pending due to weekend and today is Saturday | ".Carbon::now()->format('d-m-Y');
+                echo $action;
             }
             if($today->dayOfWeek == Carbon::SUNDAY){
                 $action = "Settlement pending due to weekend and today is Sunday | ".Carbon::now()->format('d-m-Y');
+                echo $action;
             }
 
             SettlementLog::create([
@@ -200,33 +203,38 @@ class QrisPendingWalletUpdate extends Command {
                         $qrisWalletTenant = QrisWallet::where('id_user', $stlPending->id)->where('email', $stlPending->email)->first();
                         if(!is_null($qrisWalletTenant) || empty($qrisWalletTenant)){
                             $saldoQrisTenant = $qrisWalletTenant->saldo;
-                            $qrisWalletTenant->update([
-                                'saldo' => $saldoQrisTenant+$stlPending->nominal_settle
-                            ]);
-                            $qrisWalletAdmin =  QrisWallet::where('id_user', 1)->where('email', 'adminsu@visipos.id')->first();
-                            $qrisWalletAdminSaldo = $qrisWalletAdmin->saldo;
-                            $qrisWalletAdmin->update([
-                                'saldo' => $qrisWalletAdminSaldo+$stlPending->nominal_insentif_cashback
-                            ]);
-                            SettlementHstory::create([
-                                'id_user' => $stlPending->id_user,
-                                'id_settlement' => $settlement->id,
-                                'email' => $stlPending->email,
-                                'settlement_time_stamp' => Carbon::now(),
-                                'nominal_settle' => $stlPending->nominal_settle,
-                                'nominal_insentif_cashback' => $stlPending->nominal_insentif_cashback,
-                                'status' => 1,
-                                'note' => "Settlement Pending : ".$stlPending->nomor_settlement_pending." | Scheduled on date : ".$stlPending->settlement_schedule."| is Settled On : ".Carbon::now()->format('d-m-Y'),
-                                'periode_transaksi' => $stlPending->periode_transaksi
-                            ]);
-                            $stlPending->update([
-                                'settlement_pending_status' => 1
-                            ]);
-                            SettlementLog::create([
-                                'settlement_id' => $stlPending->nomor_settlement_pending,
-                                'action' => "Settlement pending for ".$stlPending->nomor_settlement_pending." | has been settled on".Carbon::now()->format('d-m-Y')." | and today Settlement is ".$code.$dateCode,
-                                'log_timestamp' => Carbon::now(),
-                            ]);
+                            if(!is_null($saldoQrisTenant) || !empty($saldoQrisTenant)){
+                                $qrisWalletTenant->update([
+                                    'saldo' => $saldoQrisTenant+$stlPending->nominal_settle
+                                ]);
+                                $qrisWalletAdmin =  QrisWallet::where('id_user', 1)->where('email', 'adminsu@visipos.id')->first();
+                                $qrisWalletAdminSaldo = $qrisWalletAdmin->saldo;
+                                $qrisWalletAdmin->update([
+                                    'saldo' => $qrisWalletAdminSaldo+$stlPending->nominal_insentif_cashback
+                                ]);
+                                SettlementHstory::create([
+                                    'id_user' => $stlPending->id_user,
+                                    'id_settlement' => $settlement->id,
+                                    'email' => $stlPending->email,
+                                    'settlement_time_stamp' => Carbon::now(),
+                                    'nominal_settle' => $stlPending->nominal_settle,
+                                    'nominal_insentif_cashback' => $stlPending->nominal_insentif_cashback,
+                                    'status' => 1,
+                                    'note' => "Settlement Pending : ".$stlPending->nomor_settlement_pending." | Scheduled on date : ".$stlPending->settlement_schedule."| is Settled On : ".Carbon::now()->format('d-m-Y'),
+                                    'periode_transaksi' => $stlPending->periode_transaksi
+                                ]);
+                                $stlPending->update([
+                                    'settlement_pending_status' => 1
+                                ]);
+                                SettlementLog::create([
+                                    'settlement_id' => $stlPending->nomor_settlement_pending,
+                                    'action' => "Settlement pending for ".$stlPending->nomor_settlement_pending." | has been settled on".Carbon::now()->format('d-m-Y')." | and today Settlement is ".$code.$dateCode,
+                                    'log_timestamp' => Carbon::now(),
+                                ]);
+                                echo "Pending Settlement Settled - User Email : ".$stlPending->email." | ".$stlPending->nominal_settle." | Periode : ".Carbon::now()."\n";
+                            } else {
+                                echo "THis Wallet is NULL : ".$qrisWalletTenant;
+                            }
                         } else {
                             echo "Wallet not found when trying to update settlement pending and the wallet is ".$stlPending->email;
                         }
@@ -262,7 +270,7 @@ class QrisPendingWalletUpdate extends Command {
                     'email' => NULL,
                     'lokasi_anda' => 'System Report',
                     'deteksi_ip' => 'System Report',
-                    'log' => str_replace("'", "\'", json_encode(DB::getQueryLog()))."\n Error Record : ".$e,
+                    'log' => str_replace("'", "\'", json_encode(DB::getQueryLog()))."\n\n Error Record : ".$e,
                     'status' => 0
                 ]);
                 $settlement->status = 0;
